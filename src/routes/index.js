767b316412e94034145c4c9ca73c9349777ab700
SnayderCJ <jcedenoj5@unemi.edu.ec>
2024-06-09 13:03:44 -0500

    V1

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+const router = Router();
+
+router.get('/', (req, res) => res.render('index', {h1: 'First WebSite'}));
+router.get('/about', (req, res) => res.render('about', {title: 'About me'}));
+router.get('/contact', (req, res) => res.render('contact', {title: 'Contact me'}));
+
+export default router;
